3b83caaf3dae684979ace46a880dfd2dcb592dc1
蔡强 <caiqiang@gz-zhisuaninfo.com>
2024-04-08 23:42:36 +0800

    删除品牌

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -47,14 +47,14 @@ export const Badge = (props: Props) => {
       Powered by
       <a
         ref={liteBadge}
-        href={'https://flowiseai.com'}
+        href={'https://zhisuaninfo.com'}
         target="_blank"
         rel="noopener noreferrer"
         class="lite-badge"
         id="lite-badge"
         style={{ 'font-weight': 'bold', color: props.poweredByTextColor ?? defaultTextColor }}
       >
-        <span> Flowise</span>
+        <span> 智算信息 </span>
       </a>
     </span>
   );
